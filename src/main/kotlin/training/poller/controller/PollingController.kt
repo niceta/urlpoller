a54a907457b94716867d004e1b31e003baba7dd0
Nikita Kuznetsov <nikita.kuznetsov@dif.tech>
2025-04-29 10:22:09 +0300

    added exception handlers

--- src/main/kotlin/training/poller/controller/PollingController.kt
+++ src/main/kotlin/training/poller/controller/PollingController.kt
@@ -1,7 +1,9 @@
 package training.poller.controller
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.*
 import jakarta.inject.Singleton
+import training.poller.service.PollingException
 import training.poller.service.PollingService
 
 @Singleton
@@ -11,21 +13,28 @@ class PollingController(
 ) {
 
     @Put("/interval")
-    fun updateInterval(@QueryValue intervalMillis: Long) {
+    fun updateInterval(@QueryValue intervalMillis: Long): HttpResponse<Any> = try {
         pollingService.updatePollingInterval(intervalMillis)
-        println("interval has been updated")
+        HttpResponse.ok(mapOf("message" to "interval updated"))
+    } catch (error: PollingException) {
+        HttpResponse.badRequest(mapOf("error" to error.message))
     }
 
+
     @Post("/url")
-    fun addUrl(@QueryValue url: String) {
+    fun addUrl(@QueryValue url: String): HttpResponse<Any> = try {
         pollingService.addUrl(url)
-        println("url $url has been added")
+        HttpResponse.ok(mapOf("message" to "URL added"))
+    } catch (error: PollingException) {
+        HttpResponse.badRequest(mapOf("error" to error.message))
     }
 
     @Delete("/url")
-    fun removeUrl(@QueryValue url: String) {
+    fun removeUrl(@QueryValue url: String): HttpResponse<Any> = try {
         pollingService.removeUrl(url)
-        println("url $url has been removed")
+        HttpResponse.ok(mapOf("message" to "URL removed"))
+    } catch (error: PollingException) {
+        HttpResponse.badRequest(mapOf("error" to error.message))
     }
 
     @Get("/urls")
